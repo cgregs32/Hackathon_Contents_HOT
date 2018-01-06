69ade116830145a22185031206f9eb1cecc53ee4
Yurie Lorange <yl-mbp@Yuries-MacBook-Pro.local>
2018-01-06 14:15:44 -0700

    Add about page link.

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
-import { Header, Image, Segment } from 'semantic-ui-react';
+import { Header, Image, Segment, Card, Grid, List } from 'semantic-ui-react';
 
 class About extends Component {
   render() {
     return (
+      <div style={styles.homeImage}>
       <Segment basic>
       <Header as='h1' textAlign='center'>About New Golden Dragon Chinese Restaurant</Header>
       </Segment>
+      </div>
     );
+  }
+}
+
+const styles = {
+  opacity: {
+    backgroundColor: 'rgba(200, 200, 200, 0.7)',
+    height: '100vh'
+  },
+  homeImage: {
+    backgroundImage: "url(http://res.cloudinary.com/yuriecloud/image/upload/v1515259568/food_small1_qalnzz.jpg)",
+    backgroundSize: 'cover'
   }
 }
 
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -32,6 +32,9 @@ class NavBar extends Component {
         <Link to='/menu'>
           <Menu.Item name='Menu' />
         </Link>
+        <Link to='/about'>
+          <Menu.Item name='About' />
+        </Link>
         <Link to='/cart'>
           <Menu.Item name='Cart' />
         </Link>
