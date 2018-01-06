decf869421f307e004913b94c88e2978cee8b8c1
Yurie Lorange <yl-mbp@Yuries-MacBook-Pro.local>
2018-01-06 16:57:23 -0700

    add style.

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -6,27 +6,64 @@ class About extends Component {
   render() {
     return (
       <div style={styles.homeImage}>
-      <Container>
-        <Segment style={styles.opacity}>
-        <Header as='h1' textAlign='center' style={styles.textOnPage}>About New Golden Dragon Chinese Restaurant</Header>
-        <Iframe 
-        url="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3023.3160629798485!2d-111.8911381846347!3d40.733070344263275!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87528acceb3c90a9%3A0x9c09e68e403252!2s1716+S+State+St%2C+Salt+Lake+City%2C+UT+84115!5e0!3m2!1sen!2sus!4v1515276241821" 
-        width="600" 
-        height="450" 
-        frameborder="0" 
-        style="border:0" 
-        allowfullscreen />
-        </Segment>
-      </Container>
+      
+        <Container >
+          <Segment style={styles.opacity}>        
+            <Grid>
+              {/* <Grid.Row> */}
+                <Grid.Column width={6}>
+                  <div style={styles.title}>
+                    <Header as='h1' style={styles.textOnPage}>New Golden Dragon Chinese Restaurant</Header>
+                  </div>
+                </Grid.Column>
+                <Grid.Column width={10}>
+                  <div style={styles.map}>
+                    <Iframe 
+                        url="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3023.3160629798485!2d-111.8911381846347!3d40.733070344263275!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87528acceb3c90a9%3A0x9c09e68e403252!2s1716+S+State+St%2C+Salt+Lake+City%2C+UT+84115!5e0!3m2!1sen!2sus!4v1515276241821" 
+                        width="500" 
+                        height="350" 
+                        frameborder="0" 
+                        style="border:0" 
+                        allowfullscreen 
+                      />
+                  </div>
+                </Grid.Column>
+              {/* </Grid.Row> */}
+            </Grid>
+          </Segment>
+        </Container>
       </div>
     );
-  }
+  }  
 }
+
+//       <div style={styles.homeImage}>
+//       <Container>
+//         <Segment style={styles.opacity}>
+//         <div style={styles.title}>
+//           <Header as='h1' textAlign='center' style={styles.textOnPage}>About New Golden Dragon Chinese Restaurant</Header>
+//         </div>
+//         <div style={styles.map}>
+//           <Iframe 
+//           url="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3023.3160629798485!2d-111.8911381846347!3d40.733070344263275!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87528acceb3c90a9%3A0x9c09e68e403252!2s1716+S+State+St%2C+Salt+Lake+City%2C+UT+84115!5e0!3m2!1sen!2sus!4v1515276241821" 
+//           width="500" 
+//           height="350" 
+//           frameborder="0" 
+//           style="border:0" 
+//           allowfullscreen />
+//         </div>
+//         </Segment>
+//       </Container>
+//       </div>
+//     );
+//   }
+
 
 const styles = {
   opacity: {
     backgroundColor: 'rgba(200, 200, 200, 0.8)',
     height: '100vh',
+    display: 'flex',
 
   },
   homeImage: {
@@ -35,8 +72,16 @@ const styles = {
     
   },
   textOnPage: {
-    fontFamily: 'Libre Baskerville'
-  }
+    fontFamily: 'Libre Baskerville',
+    justifyContent: 'center',
+  },
+  map: {
+    marginRight: '20em',
+    justifyContent: 'flex-end'
+  },
+  title: {
+    justifyContent: 'center',
+  },
 }
 
 export default About;
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Menu } from 'semantic-ui-react';
+import { Menu, Segment } from 'semantic-ui-react';
 import { Link, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { handleLogout } from '../actions/auth';
