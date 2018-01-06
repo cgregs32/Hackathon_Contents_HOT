baca8e003bfe471f19cb28c8ad78d1c41b3e3561
Yurie Lorange <yl-mbp@Yuries-MacBook-Pro.local>
2018-01-06 15:52:44 -0700

    Add Iframe to about page.

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -10,7 +10,7 @@ class About extends Component {
         <Segment style={styles.opacity}>
         <Header as='h1' textAlign='center' style={styles.textOnPage}>About New Golden Dragon Chinese Restaurant</Header>
         <Iframe 
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3023.3160629798485!2d-111.8911381846347!3d40.733070344263275!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87528acceb3c90a9%3A0x9c09e68e403252!2s1716+S+State+St%2C+Salt+Lake+City%2C+UT+84115!5e0!3m2!1sen!2sus!4v1515276241821" 
+        url="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3023.3160629798485!2d-111.8911381846347!3d40.733070344263275!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87528acceb3c90a9%3A0x9c09e68e403252!2s1716+S+State+St%2C+Salt+Lake+City%2C+UT+84115!5e0!3m2!1sen!2sus!4v1515276241821" 
         width="600" 
         height="450" 
         frameborder="0" 
